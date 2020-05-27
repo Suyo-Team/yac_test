b7235e79ec6a4e1e332d2e54985811c73e03a262
Edgardo Obregón <edraobdu@gmail.com>
2020-05-27 17:04:26 -0500

    login endpoint added, Susscessfully generated Tokens when user registers and logs in. From previews commit, nginx routing container was removed and exposed ports for both client and server container

--- server/server/api/views.py
+++ server/server/api/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import mixins, viewsets, status
 from rest_framework import permissions
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 
 from api.serializers import UserSerializer, RegistrationSerializer
 
@@ -20,20 +20,23 @@ class UserViewSet(mixins.RetrieveModelMixin,
 
 
 @api_view(['POST'])
+@permission_classes([permissions.AllowAny])
 def registration_view(request):
 
     serializer = RegistrationSerializer(data=request.data)
     data = {}
+    status_code = status.HTTP_400_BAD_REQUEST
     if serializer.is_valid():
         new_user = serializer.save()
-        data['success'] = True
         data['response'] = "Successfully registered a new user"
         data['email'] = new_user.email
         data['username'] = new_user.username
+        data['token'] = new_user.auth_token.key
+        status_code = status.HTTP_201_CREATED
     else:
         data = serializer.errors
     
-    return Response(data)
+    return Response(data, status=status_code)
 
 
 
--- server/server/server/settings.py
+++ server/server/server/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'channels',
-    'rest_framework',    
+    'rest_framework',
+    'rest_framework.authtoken',   
     'api.apps.ApiConfig'
 ]
 
@@ -126,3 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
