529e913a744a3d9305f4b4c121f63a132568cf23
Edgardo Obregón <edraobdu@gmail.com>
2020-05-27 13:04:31 -0500

    Removed routing from nginx container to Django app

--- server/server/server/settings.py
+++ server/server/server/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    # 'channels',
+    'channels',
     'api'
 ]
 
@@ -53,6 +53,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'server.urls'
+ASGI_APPLICATION = "server.routing.application"
 
 TEMPLATES = [
     {
