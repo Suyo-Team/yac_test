19b13902dd2ac903ed0a4b5f134c5045c7af962f
Edgardo Obregón <edraobdu@gmail.com>
2020-06-03 12:02:36 -0500

    2 chat messages tests added, passing

--- server/server/api/tests/tests_views/test_chats_messages.py
+++ server/server/api/tests/tests_views/test_chats_messages.py
@@ -4,21 +4,36 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from api.serializers import ChatMessageSerializer, ChatDisplaySerializer, ChatMessageNestedSerializer
-from api.models import ChatMessage
+from api.serializers import ChatMessageSerializer, ChatMessageDisplaySerializer, ChatMessageNestedSerializer
+from api.models import ChatMessage, Chat
 
 MESSAGES_URL = reverse('api:chatmessage-list')
 
+def chat_detail_url(chat_id):
+    """ Returns the Chat detail url """
+    return reverse('api:chat-detail', args=[chat_id])
 
 def sample_message(chat, user, **params):
     """Create and return a sample chat"""
     defaults = {        
-        'chat': chat.id,
-        'user': user.id,
+        'chat_id': chat.id,
+        'user_id': user.id,
         'text': 'Sample Message'
     }
     defaults.update(params)
     new_message = ChatMessage.objects.create(**defaults)
+
+def sample_chat(*users, **params):
+    """Create and return a sample chat"""
+    defaults = {
+        'private': True,
+        'chat_name': ''
+    }
+    defaults.update(params)
+    new_chat = Chat.objects.create(**defaults)
+    new_chat.users.add(*users)
+
+    return new_chat
 
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
@@ -37,101 +52,34 @@ class PublicChatMessageApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-# class PrivateChatApiTests(TestCase):
-#     """ Test authenticated chat API access """
+class PrivateChatMessageApiTests(TestCase):
+    """ Test authenticated chat messages API access """
 
-#     def setUp(self):
-#         self.client = APIClient()
-#         self.user = create_user(**{
-#             'username': 'test',
-#             'email': 'test@test.com',
-#             'password': 'test'
-#         })
-#         self.second_user = create_user(**{
-#             'username': 'test2',
-#             'email': 'test2@test2.com',
-#             'password': 'test2'
-#         })
-        
-#         self.client.force_authenticate(user=self.user)
+    def setUp(self):
+        self.client = APIClient()
+        self.user = create_user(**{
+            'username': 'test',
+            'email': 'test@test.com',
+            'password': 'test'
+        })
+        self.second_user = create_user(**{
+            'username': 'test2',
+            'email': 'test2@test2.com',
+            'password': 'test2'
+        })
 
-#     def test_retrieve_chats(self):
-#         """ Test retrieving a list of recipes """
+        self.chat = sample_chat(*[self.user.id, self.second_user.id])        
+        self.client.force_authenticate(user=self.user)
 
-#         private_chat = sample_chat(*[self.user, self.second_user])
-#         public_chat = sample_chat(*[self.user, self.second_user], **{
-#             'private': False, 'chat_name': 'Public Chat'
-#         })
+    def test_retrieve_chats_messages(self):
+        """ Test retrieving a list of messages """
 
-#         res = self.client.get(CHATS_URL)
+        message_one = sample_message(self.chat, self.user)
+        message_two = sample_message(self.chat, self.user, **{'text': 'Second Sample Message'})
+        res = self.client.get(MESSAGES_URL)
 
-#         chats_list = Chat.objects.all()
-#         serializer = ChatSerializer(chats_list, many=True)
+        messages_list = ChatMessage.objects.all()
+        serializer = ChatMessageDisplaySerializer(messages_list, many=True)
 
-#         self.assertEqual(res.status_code, status.HTTP_200_OK)
-#         self.assertEqual(len(res.data), len(serializer.data))
-
-#     def test_retrieve_limited_to_user(self):
-#         """ Test retrieving chats only ofr active user """
-#         third_user = create_user(**{
-#             'username': 'test3',
-#             'email': 'test3@test3.com',
-#             'password': 'test3'
-#         })
-#         related_chat = sample_chat(*[self.user, self.second_user], **{
-#             'private': False, 'chat_name': 'Public Chat'
-#         })
-#         unrelated_chat = sample_chat(*[self.second_user, third_user])
-
-#         res = self.client.get(CHATS_URL)
-
-#         chats_user = Chat.objects.filter(users=self.user)
-#         serializer = ChatSerializer(chats_user, many=True)       
-
-#         self.assertEqual(res.status_code, status.HTTP_200_OK)
-#         self.assertTrue(len(res.data) == len(serializer.data) == 1)
-#         self.assertEqual(res.data, serializer.data)
-
-#     def test_public_chat_with_no_chat_name(self):
-#         """ 
-#         Test that trying to create a public chat without a 
-#         name returns a bad request
-#         """
-#         invalid_public_chat_payload = {
-#             'private': False,
-#             'chat_name': '',  # empty chat name
-#             'users': [self.user.id, self.second_user.id]
-#         }  
-#         res = self.client.post(CHATS_URL, invalid_public_chat_payload)
-
-#         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-
-#     def test_chat_with_oneself(self):
-#         """
-#         Test that trying to create a chat with onself
-#         returns a bad request
-#         """
-#         chat_with_oneself_payload = {
-#             'private': True,
-#             'chat_name': '',
-#             'users': [self.user.id, self.user.id]
-#         }
-#         res = self.client.post(CHATS_URL, chat_with_oneself_payload)
-
-#         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-
-#     def test_creating_existing_private_chat(self):
-#         """
-#         Test that creating a private chat with a user
-#         that one already has a conversation with returns a 302 
-#         error (FOUND)
-#         """
-#         existing_chat = sample_chat(*[self.user, self.second_user])
-#         existing_chat_payload = {
-#             'private': True,
-#             'chat_name': '',
-#             'users': [self.user.id, self.second_user.id]
-#         }
-#         res = self.client.post(CHATS_URL, existing_chat_payload)
-
-#         self.assertEqual(res.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), len(serializer.data))
