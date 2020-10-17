67bed815b2a9fd269a4092b519b3ff99f4c3277c
TuttoDev <jsebas2426@gmail.com>
2020-10-17 14:34:58 -0500

    se agrego el buil

---
+++ build/precache-manifest.7eac9e81e20251d718872349379b19d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d06ea46543f36da97933d95aa915f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b52c7b1dddf6e052208",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "5bd9454cf6ab29630075",
+    "url": "/static/css/main.dc6a1be0.chunk.css"
+  },
+  {
+    "revision": "4b52c7b1dddf6e052208",
+    "url": "/static/js/2.005eaa27.chunk.js"
+  },
+  {
+    "revision": "7d2282234cd0247bd4712fb2834fc8d9",
+    "url": "/static/js/2.005eaa27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd9454cf6ab29630075",
+    "url": "/static/js/main.ab5cee39.chunk.js"
+  },
+  {
+    "revision": "681e50df5c4abe6c2407",
+    "url": "/static/js/runtime-main.49aaaaed.js"
+  }
+]);
