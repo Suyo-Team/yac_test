4ee3550157032033dfbc3c4e55a81e27b6c4ee39
Richard Montoya <richard.a.montoya.l@gmail.com>
2020-10-17 03:05:16 -0500

    Add dark mode to the page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Routes from "./Routes";
+
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <Routes />
-    </>
+    </ThemeProvider>
   );
 }
